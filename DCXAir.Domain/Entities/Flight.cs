6d95c693daa76de7e35d0452f037f7ed93cb6548
Laura Sofia <laura@isoptimo.com>
2024-11-16 17:51:40 -0500

    Domain layer creation.

---
+++ DCXAir.Domain/Entities/Flight.cs
@@ -0,0 +1,10 @@
+﻿namespace DCXAir.Domain.Entities
+{
+    public class Flight
+    {
+        public Transport Transport { get; set; }
+        public string Origin { get; set; }
+        public decimal Destination { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ DCXAir.Domain/Entities/Journey.cs
@@ -0,0 +1,10 @@
+﻿namespace DCXAir.Domain.Entities
+{
+    public class Journey
+    {
+        public List<Flight> Flights { get; set; }
+        public string Origin { get; set; }
+        public decimal Destination { get; set; }
+        public string Price { get; set; }
+    }
+}
